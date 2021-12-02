256b5a5026ada53c143fac00905a18d755711fd0
Dawid Bernat <dawid.bernat91@gmail.com>
2021-12-02 12:37:08 +0100

    added more songs

---
+++ out/_next/static/1VwbkJT7EWbO5CGNoBvat/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fartists","\u002Fgenres","\u002Fsongs","\u002Fsongs\u002F[songId]","\u002Fartists\u002F[artistId]","\u002Fgenres\u002F[genreId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
